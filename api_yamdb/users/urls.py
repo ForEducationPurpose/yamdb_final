ebe0975bad4ad47b6eb4bb0a25d5530f2683e923
Макс Лобанов <lobanovmax51@gmail.com>
2023-02-05 18:25:29 +0300

    Fix name of the func with lowercase

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -3,7 +3,7 @@ from typing import List
 from django.urls import URLPattern, include, path
 
 from .routers import get_users_routes
-from .views import authenticate_send_JWT_access, sign_up_send_code
+from .views import authenticate_send_jwt_access, sign_up_send_code
 
 app_name = "users"
 
@@ -11,6 +11,6 @@ v1_users_router: List[URLPattern] = get_users_routes()
 
 urlpatterns = [
     path("auth/signup/", sign_up_send_code, name="signup"),
-    path("auth/token/", authenticate_send_JWT_access, name="token"),
+    path("auth/token/", authenticate_send_jwt_access, name="token"),
     path("", include(v1_users_router)),
 ]
